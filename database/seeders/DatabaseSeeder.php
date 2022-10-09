5c141f40236fa431e5dc4b5ba3e01bdadc460a4a
Eduardo Pessine <eduardopessine@gmail.com>
2022-10-09 15:48:36 -0300

    Scaffold inertia js

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
+        User::factory()->create([
+            'name' => fake()->name(),
+            'email' => 'assessment@cupola.com',
+            'password' => Hash::make('password'),
+        ]);
+
         Company::factory(200)->create();
     }
 }
